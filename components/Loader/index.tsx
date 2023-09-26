3ad3862c177a3df45ff8382af6c9f64a2c3bbd0b
langford-dev <langfordquarshie21@gmail.com>
2023-09-26 18:31:28 +0000

    show loader on save

--- components/Loader/index.tsx
+++ components/Loader/index.tsx
@@ -1,7 +1,10 @@
-export default function Loader(): JSX.Element {
+import { ClassValue } from "class-variance-authority/types";
+import cn from "~/utils/cn.util";
+
+export default function Loader(props: { className?: ClassValue }): JSX.Element {
   return (
     <main className="w-full h-full py-20 flex items-center justify-center">
-      <span className="loader invert"></span>
+      <span className={cn("loader", props.className)}></span>
     </main>
   );
 }
