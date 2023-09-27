71066a4583e5c452148a6a1007efeab0120b6bd1
Stephen <105071093+stephenokyere233@users.noreply.github.com>
2023-09-27 10:25:08 +0000

    change global font to inter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,22 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
 import { NextUIProvider } from "@nextui-org/react";
 
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider
-      {...pageProps}
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
-      <Toaster />
-      <NextUIProvider>
-        <Component {...pageProps} />
-      </NextUIProvider>
-    </ClerkProvider>
+    <main className={`${inter.className}`}>
+      <ClerkProvider
+        {...pageProps}
+        publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      >
+        <Toaster />
+        <NextUIProvider>
+          <Component {...pageProps} />
+        </NextUIProvider>
+      </ClerkProvider>
+    </main>
   );
 }
